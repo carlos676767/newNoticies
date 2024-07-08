dcbb19a5f2b5226a4188d6dd7aa029242a07ae4b
carloshigorbr12 <carlitobr730@yahoo.com>
2024-07-08 17:36:35 -0400

    ok

---
+++ bot/app.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const httpRequest = async () => {
+  try {
+    const httpData = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    console.log(httpData.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+console.log('hello');
