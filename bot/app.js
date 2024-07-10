5bf603e19862f7dfbdb327590781db1b5ebd6032
carlos676767 <carlitobr730@gmail.com>
2024-07-09 17:01:56 -0700

    ok

--- bot/app.js
+++ bot/app.js
@@ -15,32 +15,36 @@ client.once('ready', async() => {
   const channel = client.channels.cache.get(config.idCanal)
   if (channel) {
     await httpRequest(data => {
-    const {title, urlToImage,content } = data.data.articles[0]
-    channel.send({ embeds: [embedes(title, urlToImage, content)] });
+    const {title, urlToImage,content,url } = data.data.articles[randomNoticies()]
+    channel.send({ embeds: [embedes(title, urlToImage, content, url)] });
     })
   }
 })
 
 const httpRequest = async (sucesso) => {
   try {
-    const httpData = await axios.get(`https://newsapi.org/v2/top-headlines?apiKey=${config.apiKey}&language=en&pageSize=1`)
+    const httpData = await axios.get(`https://newsapi.org/v2/top-headlines?apiKey=${config.apiKey}&language=en&pageSize=50`)
     sucesso(httpData)
   } catch (error) {
     console.log(error);
   }
 };
 
-
-const embedes = (title, urlToImage, conteudo) => {
+const embedes = (title, urlToImage, conteudo, url) => {
   const embed =  new EmbedBuilder()
   .setColor("#0099ff")
   .setTitle(`\n${title}`)
   .setThumbnail(urlToImage)
-  .setDescription(conteudo);
+  .setDescription(conteudo)
+  .setURL(url)
   return embed
 
 }
 
+
+function randomNoticies() {
+  return Math.floor(Math.random() *50)
+}
 
 (async() => {
  await client.login(config.token)
